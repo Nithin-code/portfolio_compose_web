ed957cd7ae93ddbeb49a9e3930d37c64436d7887
Nithin_Dell_G15 <shanmukhsainithin@gmail.com>
2025-08-31 01:57:02 +0530

    skills added

--- composeApp/src/commonMain/kotlin/com/nithin/portfolio/utils/SkillDetails.kt
+++ composeApp/src/commonMain/kotlin/com/nithin/portfolio/utils/SkillDetails.kt
@@ -6,15 +6,15 @@ enum class SkillDetails(
 
     KOTLIN("Kotlin"),
     JETPACK_COMPOSE("Jetpack Compose"),
-    MVVM("MVVM"),
-    ANDROID("ANDROID"),
-    JAVA("JAVA"),
+    MVVM("MVVM - MVI"),
+    ANDROID("Android"),
+    JAVA("Java"),
     CMM("Compose Multi-Platform"),
     KMP("KMP"),
     FIREBASE("Firebase"),
     MONGO_DB("MongoDb"),
     Coroutines("Coroutines"),
-    FLOWS("FLOWS"),
-    GIT("GIT"),
+    FLOWS("Flows"),
+    GIT("Git"),
     FLUTTER("Flutter")
 }
